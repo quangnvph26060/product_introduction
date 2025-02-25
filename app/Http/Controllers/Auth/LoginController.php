2d41b7df5e26cf54628c62afeb23e35266fc2c67
thangngo.dev <thangngo130802@gmail.com>
2025-02-25 16:06:20 +0700

    up

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,22 +20,24 @@ class LoginController extends Controller
         $request->validate([
             'email' => 'required',
             'password' => 'required',
+            'g-recaptcha-response' => 'required',
         ], [
            'email.required' => 'Vui lòng nhập nhập email',
             'password.required' => 'Vui lòng nhập mật khẩu.',
+            'g-recaptcha-response.required' => 'Vui lòng nhập chọn reCaptcha.',
         ]);
-        
+
         $credentials = $request->only('email', 'password');
         if (!Auth::attempt($credentials)) {
             return redirect()->route('login')->withErrors(trans('auth.failed'));
         }
-        
+
         $user = Auth::user();
-        
+
         return $this->authenticated($request, $user);
     }
 
-    protected function authenticated(Request $request, $user) 
+    protected function authenticated(Request $request, $user)
     {
         return redirect()->route('dashboard');
     }
